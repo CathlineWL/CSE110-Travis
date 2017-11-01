a548d590fc899bfe55b1b00806222dd4837384fa
CathlineWL <cathlinewl0113@gmail.com>
2017-11-01 13:19:26 -0700

    message

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -14,13 +14,13 @@ public class Celsius extends Temperature
 
     @Override
     public Temperature toFahrenheit() {
-        float value = (this.getValue()*9)/5 + 32;
+        float value = ((this.getValue()*9)/5)+ 32;
         return new Fahrenheit(value);
     }
 
     public String toString()
     {
         // TODO: Complete this method
-        return "";
+        return this.getValue()+"C";
     }
 }
